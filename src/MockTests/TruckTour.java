9612ff7c70465f3a02d951d363ebb28c74a6eaa9
Abdus Hannan <aah120@LAPTOP-LNJGL3J3.localdomain>
2022-09-20 18:35:13 +0100

    complete mock test 4

---
+++ src/MockTests/TruckTour.java
@@ -0,0 +1,29 @@
+package MockTests;
+
+import java.util.List;
+
+public class TruckTour {
+    
+    public static int truckTour(List<List<Integer>> petrolpumps) {
+
+        for (int i = 0; i < petrolpumps.size(); i++) { 
+            int tank = 0;
+            int startingPump = i;
+
+            for (int j = 0; j < petrolpumps.size(); j++) {
+                int currPump = (startingPump + j) % petrolpumps.size();
+                tank += petrolpumps.get(currPump).get(0) - petrolpumps.get(currPump).get(1);
+                if (tank < 0) {
+                    break;
+                }
+
+                if (j == petrolpumps.size() - 1) {
+                    return startingPump;
+                }
+            }
+            
+        }
+
+        return -1;
+    }
+}
